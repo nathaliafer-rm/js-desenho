f0dcf0ee6d013c44ef03c4ecf6920c055a328b84
nathaliafer-rm <nathalia.mafra@escola.pr.gov.br>
2024-07-31 07:57:57 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,38 @@
+function setup() {
+    createCanvas(600, 600);
+  }
+  function draw() {
+   background("rgb(147,160,231)")
+    stroke("white")
+    fill("white")
+    square(50,40,90)
+    square(10,75,55)
+    square(130,75,55);
+    stroke("white")
+    fill("white")
+    square(330,50,85)
+    square(300,80,55)
+    square(400,80,55);
+    fill("green") 
+    stroke ("green")
+    rect(0,400,600,600)
+    fill("brown")
+    stroke ("brown")
+    rect(200,250,5,150)
+    fill("rgb(252,133,198)")
+    stroke("rgb(220,133,198)")
+    circle(170,250,50)
+    fill("rgb(252,133,198)")
+    stroke("rgb(220,133,198)")
+    circle(235,250,50)
+    fill("rgb(252,133,198)")
+    stroke("rgb(220,133,198)")
+    circle(200,280,50)
+    fill("rgb(252,133,198)")
+    stroke("rgb(220,123,181)")
+    circle(200,220,50)
+    fill("yellow")
+    stroke("yellow")
+    circle(200,250,50) 
+  }
+  
